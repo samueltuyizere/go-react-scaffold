9ff789b9884516385a7e9d85ce57bc6de354a46a
TUYIZERE Samuel <samueltuyizere0@gmail.com>
2024-12-31 18:11:09 +0200

    configured prettier

---
+++ frontend/.prettierrc.mjs
@@ -0,0 +1,17 @@
+const config = {
+  semi: false, // Use semicolons at the end of statements.
+  singleQuote: true, // Prefer single quotes over double quotes.
+  tabWidth: 2, // Set the number of spaces per indentation level.
+  useTabs: false, // Use spaces for indentation instead of tabs.
+  trailingComma: 'none', // Avoid trailing commas in objects, arrays, etc.
+  arrowParens: 'always', // Always include parentheses around arrow function parameters.
+  printWidth: 80, // Wrap lines that exceed 80 characters.
+  endOfLine: 'lf', // Use line feed ('\n') for new lines.
+  bracketSpacing: true, // Add spaces between brackets in object literals.
+  jsxSingleQuote: false, // Use double quotes in JSX.
+  quoteProps: 'as-needed', // Only add quotes around object properties when necessary.
+  htmlWhitespaceSensitivity: 'css', // Respect CSS display property for whitespace handling.
+  jsxBracketSameLine: false // Place closing JSX bracket on a new line.
+}
+
+export default config
